8c0a06ffbd206a2897c9cebbf3622311b2bad635
John Colagioia <john@colagioia.net>
2023-03-03 06:36:37 -0500

    Fix test script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 convert -size 100x100 xc:#dccea1 test.png
-ruby schedule.rb --content-warning 'Test Message - Please Ignore' --description 'A yellow-ish square' --image-file 'test.png' --status 'Ignore me.  I am testing a script, and will probably delete this before you can finish your reply, assuming that it posts at all...' --sensitive
+ruby schedule.rb --content-warning "Test Message - Please Ignore" --description "A yellow-ish square" --image-file "test.png" --status "Ignore me.  I am testing a script, and will probably delete this before you can finish your reply, assuming that it posts at all...\n$(date)" --sensitive
 
