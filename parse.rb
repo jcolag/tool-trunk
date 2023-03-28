ca23a7afaf58974f685a57a26357fc9a02f50a7d
John Colagioia <john@colagioia.net>
2023-03-28 06:47:31 -0400

    Clean up code, making Rubocop happy

--- parse.rb
+++ parse.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'English'
 require 'date'
 require 'json'
 require 'optparse'
@@ -12,7 +13,6 @@ class Options
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = 'Usage:  schedule.rb [options]'
-      opts.separator ''
       opts.separator 'Specific options:'
       opts.on('-f', '--file FILE', 'The blog post to parse') do |i|
         options.file = i
@@ -25,27 +25,29 @@ class Options
 end
 
 def parse_date(heading)
-  tz = (Time.now + 6 * 86400).utc_offset / 36
+  tz = (Time.now + (6 * 86_400)).utc_offset / 36
   part = heading.split
   param = {}
 
   return param if part.length < 7
 
   dt = DateTime.parse "#{part[6]} #{part[5]} #{part[4]} #{part[1]}#{tz}"
-  param[:time] = dt.iso8601.sub /.*\K:/, ''
+
+  param[:time] = dt.iso8601.sub(/.*\K:/, '')
   param
 end
 
 def build_status(params)
-  return "#{params[:quote]}\n\n#{params[:cite]}\n\n#{params[:hashtags]}" unless params[:cite].nil?
+  return if params[:quote].nil?
+  return "#{params[:quote].gsub "\n\n", "\n"}\n\n#{params[:cite]}\n\n#{params[:hashtags]}" unless params[:cite].nil?
 
   "#{params[:title]} #{params[:url]}\n\n#{params[:quote].strip}\n\n#{params[:hashtags]}"
 end
 
 def fmt_args(command, params)
-  result = command.split ' '
+  result = command.split
 
-  params.keys.each do |k|
+  params.each_key do |k|
     result.push "--#{k.to_s.gsub '_', '-'}"
     result.push "\"#{params[k]}\""
   end
@@ -58,9 +60,7 @@ def process(params)
 
   args = fmt_args 'ruby schedule.rb', params
   system args.join ' '
-  unless $? == 0
-    puts args.join ' '
-  end
+  puts args.join ' ' unless $CHILD_STATUS == 0
 end
 
 options = Options.parse ARGV
@@ -69,6 +69,7 @@ parameters = {}
 File.open(options.file).each_line do |line|
   if line.start_with? '## '
     parameters[:status] = build_status parameters
+
     process parameters.except(:cite, :hashtags, :quote, :title, :url)
     parameters = parse_date line
     break if parameters[:time].nil?
@@ -76,6 +77,7 @@ File.open(options.file).each_line do |line|
     parameters[:content_warning] = line.split[2..-2].join ' '
   elsif line.start_with? '{% embed '
     part = line.split[2..-2].join(' ').split '|'
+
     parameters[:image_url] = part[0]
     parameters[:description] = part[1]
     parameters[:sensitive] = true unless part[2] == 'false'
@@ -88,7 +90,7 @@ File.open(options.file).each_line do |line|
       parameters[:url] = link.split('(')[1].split(')')[0]
     end
   elsif line.start_with? ' > '
-    parameters[:quote] = line.slice 3, line.length
+    parameters[:quote] += "\n#{line.slice(3, line.length)}"
   elsif line.start_with? '{% cite '
     parameters[:cite] = line.split[2..-2].join ' '
   elsif line.start_with? 'Hashtags: '
