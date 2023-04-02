cfb4d4a005011910e8d162a1b04a081427d0d81c
John Colagioia <john@colagioia.net>
2023-04-02 06:33:31 -0400

    Add some rudimentary JavaScript

---
+++ mastodon.js
@@ -0,0 +1,22 @@
+let config = {};
+
+window.addEventListener('load', (e) => {
+  const modal = document.getElementById('startup-modal');
+  const col = document.getElementById('timeline');
+
+  fetch('./rummage.json')
+    .then((response) => {
+      response.arrayBuffer()
+        .then(setConfig);
+    });
+});
+
+function setConfig(response) {
+  const modal = document.getElementById('startup-modal');
+  const decoder = new TextDecoder('utf-8');
+  const json = decoder.decode(response);
+
+  config = JSON.parse(json);
+  modal.classList.add('hidden-modal');
+}
+
