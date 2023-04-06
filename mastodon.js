ff60e3d9bf1383f6490a203ae9ddd7f009ef5ccb
John Colagioia <john@colagioia.net>
2023-04-06 06:26:18 -0400

    Add utility function to make web requests

--- mastodon.js
+++ mastodon.js
@@ -21,5 +21,27 @@ function setConfig(response) {
 
   config = JSON.parse(json);
   modal.classList.add('hidden-modal');
+
+// Adapted from https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
+function httpGet(url) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.onload = (e) => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        timeline = JSON.parse(xhr.responseText);
+      } else {
+        console.error(xhr.statusText);
+        return null;
+      }
+    }
+  };
+  xhr.onerror = (e) => {
+    console.error(xhr.statusText);
+    return null;
+  };
+  xhr.send(null);
+  latch = false;
 }
 
