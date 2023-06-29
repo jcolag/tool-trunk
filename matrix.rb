8ff18c456fd88443da827f88bb8baaa900b67c50
John Colagioia <john@colagioia.net>
2023-06-29 06:45:03 -0400

    Clean up Matrix operations

--- matrix.rb
+++ matrix.rb
@@ -3,12 +3,19 @@
 require 'matrix_sdk'
 
 # Post a message to a specified chatroom on Matrix.
-config_file = File.open 'matrix.json'
+config_name = File.join Dir.home, '.config', 'matrix.json'
+config_file = File.open config_name
 config = JSON.parse config_file.read
 client = MatrixSdk::Client.new config['server']
 
 client.login config['user'], config['password']
 
 room = client.find_room config['room']
+
+if room.nil?
+  puts "\nUser #{config['user']} doesn't appear to have " \
+       "access to #{config['room']}.\n\n"
+  return
+end
 
 room.send_text ARGV.join(' ')
